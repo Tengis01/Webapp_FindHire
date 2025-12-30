c834427c4de39aed54a758e29cc042f662473fb1
Tengis01 <bad.t1131@gmail.com>
2025-12-30 22:50:32 +0800

    Add cookie session complate sign in/up

---
+++ api/models/User.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
+    address: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ['User', 'Worker'], default: 'User' },
+    createdAt: { type: Date, default: Date.now }
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
